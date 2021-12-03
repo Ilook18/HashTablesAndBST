688de3bbe14ac8c7c2da937fdd3eab7ceab380c7
“Enter <anirudhanand984@gmail.com>
2021-12-04 02:48:44 +0530

    [Anirudh] add : Created Binary Search Tree

--- HashTablesAndBST/HashTablesAndBST/Program.cs
+++ HashTablesAndBST/HashTablesAndBST/Program.cs
@@ -6,16 +6,16 @@ namespace HashTablesAndBST
     {
         static void Main(string[] args)
         {
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
             bool flag = true;
             while (flag)
             {
                 Console.WriteLine("Welcome to Hash Tables and BST Programs");
-                Console.WriteLine("Choose the program to be executed : \n 1. Sentence Frequency \n 2. Exit");
+                Console.WriteLine("Choose the program to be executed : \n 1. Sentence Frequency \n 2. Binary Search Tree Creation \n 3. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
+                        MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
                         hash.Add("0", "To");
                         hash.Add("1", "be");
                         hash.Add("2", "or");
@@ -25,9 +25,16 @@ namespace HashTablesAndBST
                         string hash5 = hash.Get("5");
                         Console.WriteLine("5th index Value is : {0}" , hash5);
                         hash.Remove("1");
-                        
+                      
                         break;
                     case 2:
+                        Console.WriteLine(" Binary Search Tree");
+                        BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+                        binarySearch.Insert(30);
+                        binarySearch.Insert(70);
+                        binarySearch.Display();
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
